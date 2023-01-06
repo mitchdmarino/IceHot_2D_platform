e932fe46492e69d318084353780ce1abc001ac36
mitchdmarino <mitchdmarino@gmail.com>
2023-01-06 15:05:17 -0800

    Level1 created and changing levels available

---
+++ scripts/Level_2.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#$YellowObstacle/YellowButton.translate(Vector2(-450,0))
+	#$BlueObstacle/YellowWall/Sprite.modulate = Color(0, 0, 1)
+	#$BlueObstacle/YellowButton/ButtonAnimation.modulate = Color(0, 0, 1)
+	#$BlueObstacle/YellowButton.translate(Vector2(500, -70))
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,81 +1,85 @@
 extends Node
 var blueControl = true
 # Called when the node enters the scene tree for the first time
-
+var level = self.get_child(0)
 func _ready():
-	var yellowButton = $YellowObstacle/YellowButton.duplicate()
-	yellowButton.translate(Vector2(100, -500))
-	$YellowObstacle.add_child(yellowButton)
-	$YellowObstacle/YellowButton.translate(Vector2(-100, 0))
-	$YellowObstacle/YellowWall/Sprite.modulate = Color(0, 0, 1)
-	$YellowObstacle/YellowButton/ButtonAnimation.modulate = Color(0, 0, 1)
+		pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (blueControl):
 		if Input.is_action_pressed("ui_right"):
-			$IcePlayer.moveRight = true
+			$Level_1/IcePlayer.moveRight = true
 		if Input.is_action_just_released("ui_right"):
-			$IcePlayer.moveRight = false
+			$Level_1/IcePlayer.moveRight = false
 		if Input.is_action_pressed("ui_left"):
-			$IcePlayer.moveLeft = true
+			$Level_1/IcePlayer.moveLeft = true
 		if Input.is_action_just_released("ui_left"):
-			$IcePlayer.moveLeft = false
+			$Level_1/IcePlayer.moveLeft = false
 		if Input.is_action_just_pressed("ui_up"):
-			$IcePlayer.jump = true
+			$Level_1/IcePlayer.jump = true
 			yield(get_tree().create_timer(0.3), "timeout")
-			$IcePlayer.jump = false 
+			$Level_1/IcePlayer.jump = false 
 	else:
 		if Input.is_action_pressed("ui_right"):
-			$FirePlayer.moveRight = true
+			$Level_1/FirePlayer.moveRight = true
 		if Input.is_action_just_released("ui_right"):
-			$FirePlayer.moveRight = false
+			$Level_1/FirePlayer.moveRight = false
 		if Input.is_action_pressed("ui_left"):
-			$FirePlayer.moveLeft = true
+			$Level_1/FirePlayer.moveLeft = true
 		if Input.is_action_just_released("ui_left"):
-			$FirePlayer.moveLeft = false
+			$Level_1/FirePlayer.moveLeft = false
 		if Input.is_action_just_pressed("ui_up"):
-			$FirePlayer.jump = true
+			$Level_1/FirePlayer.jump = true
 			yield(get_tree().create_timer(0.3), "timeout")
-			$FirePlayer.jump = false 
+			$Level_1/FirePlayer.jump = false 
 
 func _on_HUD_move_left():
 	if (blueControl):
-		$IcePlayer.moveLeft = true
+		$Level_1/IcePlayer.moveLeft = true
 	else:
-		$FirePlayer.moveLeft = true
+		$Level_1/FirePlayer.moveLeft = true
 
 func _on_HUD_move_right():
 	if (blueControl):
-		$IcePlayer.moveRight = true
+		$Level_1/IcePlayer.moveRight = true
 	else:
-		$FirePlayer.moveRight = true
+		$Level_1/FirePlayer.moveRight = true
 
 func _on_HUD_stop_move_left():
 	if (blueControl):
-		$IcePlayer.moveLeft = false
+		$Level_1/IcePlayer.moveLeft = false
 	else:
-		$FirePlayer.moveLeft = false
+		$Level_1/FirePlayer.moveLeft = false
 
 func _on_HUD_stop_move_right():
 	if (blueControl):
-		$IcePlayer.moveRight = false
+		$Level_1/IcePlayer.moveRight = false
 	else:
-		$FirePlayer.moveRight = false
+		$Level_1/FirePlayer.moveRight = false
 
 func _on_HUD_jump():
 	if (blueControl):
-		$IcePlayer.jump = true
+		$Level_1/IcePlayer.jump = true
 		yield(get_tree().create_timer(0.3), "timeout")
-		$IcePlayer.jump = false
+		$Level_1/IcePlayer.jump = false
 	else:
-		$FirePlayer.jump = true
+		$Level_1/FirePlayer.jump = true
 		yield(get_tree().create_timer(0.3), "timeout")
-		$FirePlayer.jump = false
+		$Level_1/FirePlayer.jump = false
 
 func _on_WaterWay_player_entered():
 	pass
 
 func _on_HUD_switch_control():
 	blueControl = !blueControl
+
+
+func _on_Level_1_level_complete():
+	var level_1 = self.get_node("Level_1")
+	self.remove_child(level_1)
+	var level_2_resource = load("res://Level_2.tscn")
+	var level_2 = level_2_resource.instance()
+	self.add_child(level_2)
+	
